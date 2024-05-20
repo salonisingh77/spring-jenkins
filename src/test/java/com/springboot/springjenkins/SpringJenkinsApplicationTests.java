4a036faa706670f12be1c8a09063afb7292f7bc2
Saloni <asaloni483@gmail.com>
2024-05-20 21:36:57 +0530

    first commit

---
+++ src/test/java/com/springboot/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.springboot.springjenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	public static Logger Logger=LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		Logger.info("Test case executed...");
+		assertEquals(true,true);
+	}
+
+}
